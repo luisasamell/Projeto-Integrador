d0745d57faaae5bd173f62e1ccd5358e4c377e5e
luisasamell <162448878+luisasamell@users.noreply.github.com>
2024-03-20 10:03:57 -0300

    Add files via upload

---
+++ Exemplo Luísa.py
@@ -0,0 +1,40 @@
+print ("LUISA")
+numero = 10
+nome = "Luisa"
+booleano = True
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+numero1 = 2
+numero2 = 4
+numero3 = 6
+numero4 = 8
+frase = "hoje foi muito legal"
+palavra = "linda"
+
+media = (numero1 + numero2 + numero3 + numero4) /4
+print("A média é", media)
+
+quadrado = numero3 * numero3
+print ("O quadrado do número 6 é", quadrado)
+
+dobro = numero4 * 2
+print ("O dobro do número 8 é", dobro)
+
+frase = "hoje foi muito legal"
+quantidade_letras = len (frase)
+print ("A frase tem", quantidade_letras, "letras.")
+
+quantidade_espacos = frase.count(" ")
+print ("A quantidade de espaços na frase é", quantidade_espacos)
+
+if numero1 > numero2:
+    print ("O numero1 é maior que o numero2.")
+else:
+               print ("o numero3 é maior que o numero4")
+
+maior_numero = max (numero1, numero2, numero3, numero4)
+print ("O maior número é:", maior_numero)
