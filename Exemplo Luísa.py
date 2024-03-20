2bda6e4c564bfee3ccb22c6b6763a3c3968ff4b2
luisasamell <162448878+luisasamell@users.noreply.github.com>
2024-03-20 10:05:55 -0300

    Luísa.py

--- Exemplo Luísa.py
+++ Exemplo Luísa.py
@@ -38,3 +38,167 @@ else:
 
 maior_numero = max (numero1, numero2, numero3, numero4)
 print ("O maior número é:", maior_numero)
+
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+2
+4
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+Nome
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    Nome
+NameError: name 'Nome' is not defined. Did you mean: 'nome'?
+nome
+'Luisa'
+número
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    número
+NameError: name 'número' is not defined. Did you mean: 'numero'?
+numero
+10
+booleano
+True
+numero x 2
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+numero * 2
+20
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+numero
+10
+nome
+'Luisa'
+booleano
+True
+a
+10
+b
+5
+soma
+15
+subtracao
+5
+multiplicacao
+50
+divisao
+2.0
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+Traceback (most recent call last):
+  File "C:/Users/202104182562/Downloads/exemplo.py", line 11, in <module>
+    numero2
+NameError: name 'numero2' is not defined. Did you mean: 'numero'?
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+numero1
+2
+numero2
+4
+numero3
+6
+numero4
+8
+frase
+'hoje foi muito legal'
+palavra
+'linda'
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número é 36
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+A quantidade de espaços na frase é 3
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+A quantidade de espaços na frase é 3
+o numero3 é maior que o numero4
+
+maior_numero = max (numero1,numero2,numero3,numero4)
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+A quantidade de espaços na frase é 3
+o numero3 é maior que o numero4
+O maior número é:
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+A quantidade de espaços na frase é 3
+o numero3 é maior que o numero4
+O maior número é:
+
+============== RESTART: C:/Users/202104182562/Downloads/exemplo.py =============
+LUISA
+A média é 5.0
+O quadrado do número 6 é 36
+O dobro do número 8 é 16
+A frase tem 20 letras.
+A quantidade de espaços na frase é 3
+o numero3 é maior que o numero4
+O maior número é: 8
